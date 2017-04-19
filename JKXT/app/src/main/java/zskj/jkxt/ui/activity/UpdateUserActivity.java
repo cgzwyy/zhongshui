7a076ddc465e640ea7ede6744231424869e8e326
hanguoxin <guoxin.han@vhall.com>
2017-04-19 18:09:41 +0800

    UpdateUserActivity

--- JKXT/app/src/main/java/zskj/jkxt/ui/activity/UpdateUserActivity.java
+++ JKXT/app/src/main/java/zskj/jkxt/ui/activity/UpdateUserActivity.java
@@ -23,27 +23,31 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import zskj.jkxt.R;
 import zskj.jkxt.WebService;
 import zskj.jkxt.domain.User;
 
-public class UpdateUserActivity extends AppCompatActivity {
+public class UpdateUserActivity extends AppCompatActivity implements View.OnClickListener {
 
-    User mUser, newUser;
+    User mUser;
+    //账号密码
     TextView update_userName;
-    EditText update_userPassword;
+    EditText update_userPassword;//密码
+    //权限
     CheckBox update_rightsStation, update_rightsPower, update_rightsAlarm;
-    Map<String, Boolean> update_right = new HashMap<>();
+    //用户级别
     RadioGroup update_userLevel;
     RadioButton update_rbLevelOne, update_rbLevelTwo;
-    String update_level;
-    Map<String, Boolean> update_range = new HashMap<>();
-    RadioGroup update_userRange;
-    CheckBox update_rangeStation;
+    //管理范围
+    LinearLayout update_userRange;
+    String[] stations;//所有 站点
+    //按钮
     Button update_update, update_cancel;
     UpdateUserTask mTask;
 
@@ -52,219 +56,129 @@ public class UpdateUserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_user);
         mUser = (User) getIntent().getSerializableExtra("user");
+//        mUser = new User();
+//        mUser.userId = 1;
+//        mUser.userLevel = "1";
+//        mUser.userName = "test";
+//        mUser.userPassword = "123456";
+//        mUser.userRights = "场站,全场功率";
+//        mUser.userRange = "托克逊,布尔津,呵呵";
 
-        newUser = mUser;
 
+        initView();
+        initData();
+    }
+
+    private void initView() {
         update_userName = (TextView) this.findViewById(R.id.update_userName);
-        update_userName.setText(mUser.userName);
-
         update_userPassword = (EditText) this.findViewById(R.id.update_userPassword);
-        update_userPassword.setText(mUser.userPassword);
-        update_userPassword.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                newUser.userPassword = update_userPassword.getText().toString();
-            }
-        });
-
-        if (update_right == null) {
-            update_right = new HashMap<>();
-        }
-        update_right.clear();
-        String[] userRights = mUser.userRights.split(",");
-        for (int i = 0; i < userRights.length; i++) {
-            update_right.put(userRights[i], true);
-        }
-
         update_rightsStation = (CheckBox) this.findViewById(R.id.update_rightsStation);
         update_rightsPower = (CheckBox) this.findViewById(R.id.update_rightsPower);
         update_rightsAlarm = (CheckBox) this.findViewById(R.id.update_rightsAlarm);
-        if (update_right.containsKey(update_rightsStation.getText().toString()))
-            update_rightsStation.setChecked(true);
-        update_rightsStation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    update_right.put(update_rightsStation.getText().toString(), true);
-                } else {
-                    update_right.remove(update_rightsStation.getText().toString());
-                }
-            }
-        });
-        if (update_right.containsKey(update_rightsPower.getText().toString()))
-            update_rightsPower.setChecked(true);
-        update_rightsPower.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    update_right.put(update_rightsPower.getText().toString(), true);
-                } else {
-                    update_right.remove(update_rightsPower.getText().toString());
-                }
-            }
-        });
-        if (update_right.containsKey(update_rightsAlarm.getText().toString()))
-            update_rightsAlarm.setChecked(true);
-        update_rightsAlarm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    update_right.put(update_rightsAlarm.getText().toString(), true);
-                } else {
-                    update_right.remove(update_rightsAlarm.getText().toString());
-                }
-            }
-        });
 
+        update_userLevel = (RadioGroup) this.findViewById(R.id.update_userLevel);
         update_rbLevelOne = (RadioButton) this.findViewById(R.id.update_rbLevelOne);
         update_rbLevelTwo = (RadioButton) this.findViewById(R.id.update_rbLevelTwo);
-        if (mUser.userLevel.equals("1")) {
-            update_rbLevelOne.setChecked(true);
-            update_level = update_rbLevelOne.getText().toString();
-        } else {
-            update_rbLevelTwo.setChecked(true);
-            update_level = update_rbLevelTwo.getText().toString();
-        }
-        update_userLevel = (RadioGroup) this.findViewById(R.id.update_userLevel);
-        update_userLevel.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton radioButton = (RadioButton) findViewById(update_userLevel.getCheckedRadioButtonId());
-                update_level = radioButton.getText().toString();
-            }
-        });
-        if (update_range == null) {
-            update_range = new HashMap<>();
-        }
-        update_range.clear();
-        String[] userRanges = mUser.userRange.split(",");
-        for (int i = 0; i < userRanges.length; i++) {
-            update_range.put(userRanges[i], true);
-        }
-        update_userRange = (RadioGroup) this.findViewById(R.id.update_userRange);
-        String stations = "";
-        try {
-            InputStream in = getResources().openRawResource(R.raw.stations);//获取
-            int length = in.available();
-            byte[] buffer = new byte[length];
-            in.read(buffer);
-            stations = new String(buffer, "UTF-8");
-            in.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
-        if (!TextUtils.isEmpty(stations)) {
-            String station[] = stations.split(";");
-            for (int i = 0; i < station.length; i++) {
-                final String[] module = station[i].split(",", 3);
-                if (i == 0) {
-                    update_rangeStation = (CheckBox) this.findViewById(R.id.update_rangeStation1);
-                    update_rangeStation.setText(module[0]);
-                    update_rangeStation.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);
-                    update_rangeStation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                        @Override
-                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                            if (isChecked) {
-                                update_range.put(module[0], true);
-                            } else {
-                                update_range.remove(module[0]);
-                            }
-                        }
-                    });
-                } else {
-                    update_rangeStation = new CheckBox(this);
-                    update_rangeStation.setText(module[0]);
-                    update_rangeStation.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);
-                    update_rangeStation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                        @Override
-                        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                            if (isChecked) {
-                                Log.e("Listener2--->", "checked");
-                                update_range.put(module[0], true);
-                            } else {
-                                Log.e("Listener2--->", "unchecked");
-                                update_range.remove(module[0]);
-                            }
-                        }
-                    });
-                    update_userRange.addView(update_rangeStation, LinearLayout.LayoutParams.WRAP_CONTENT,
-                            LinearLayout.LayoutParams.WRAP_CONTENT);
-                }
-                if (update_range.containsKey(module[0])) {
-                    update_rangeStation.setChecked(true);
-                }
+        update_userRange = (LinearLayout) this.findViewById(R.id.update_userRange);
+        getStation();
+        if (stations != null && stations.length > 0) {
+            for (int i = 0; i < stations.length; i++) {
+                CheckBox child = new CheckBox(this);
+                child.setText(stations[i]);
+                child.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);
+                update_userRange.addView(child);
             }
         }
 
         update_update = (Button) this.findViewById(R.id.update_update);
         update_cancel = (Button) this.findViewById(R.id.update_cancel);
-        update_cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-        update_update.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String userRange = "";
-                Iterator iterator = update_range.keySet().iterator();
-                while (iterator.hasNext()) {
-                    String key = (String) iterator.next();
-                    userRange += key + ",";
-                }
-                if (update_range.size() == 0) {
-                    newUser.userRange = "";
-                } else {
-                    newUser.userRange = userRange.substring(0, userRange.length() - 1);
-                }
 
-                if (update_level.equals("超级用户")) {
-                    newUser.userLevel = "1";
-                } else {
-                    newUser.userLevel = "2";
-                }
+        update_update.setOnClickListener(this);
+        update_cancel.setOnClickListener(this);
 
-                String userRights = "";
-                Iterator iterator2 = update_right.keySet().iterator();
-                while (iterator2.hasNext()) {
-                    String key = (String) iterator2.next();
-                    userRights += key + ",";
-                }
-                if (update_right.size() == 0) {
-                    newUser.userRights = "";
-                } else {
-                    newUser.userRights = userRights.substring(0, userRights.length() - 1);
-                }
-                if (newUser.userName.equals("")) {
-                    Toast.makeText(getApplicationContext(), "用户名不能为空", Toast.LENGTH_LONG).show();
-                    return;
-                } else if (newUser.userRights.equals("")) {
-                    Toast.makeText(getApplicationContext(), "权限不能为空", Toast.LENGTH_LONG).show();
-                    return;
-                } else if (newUser.userRange.equals("")) {
-                    Toast.makeText(getApplicationContext(), "范围不能为空", Toast.LENGTH_LONG).show();
-                    return;
-                }
-//                Toast.makeText(getApplicationContext(), mUser.userName +"\n" + mUser.userPassword +
-//                        "\n" + confirmPassword + "\n" + mUser.userRights + "\n" + mUser.userRange +
-//                        "\n" + mUser.userLevel, Toast.LENGTH_LONG).show();
-
-                updateUser(newUser.userName, newUser.userPassword, newUser.userRights, newUser.userRange, newUser.userLevel);
-            }
-        });
     }
+
+    private void initData() {
+        update_userName.setText(mUser.userName);
+        update_userPassword.setText(mUser.userPassword);
+        String[] userRights = mUser.userRights.split(",");
+        if (userRights != null && userRights.length > 0) {
+            for (int i = 0; i < userRights.length; i++) {
+                if (userRights[i].equals(update_rightsStation.getText().toString())) {
+                    update_rightsStation.setChecked(true);
+                    continue;
+                }
+                if (userRights[i].equals(update_rightsPower.getText().toString())) {
+                    update_rightsPower.setChecked(true);
+                    continue;
+                }
+                if (userRights[i].equals(update_rightsAlarm.getText().toString())) {
+                    update_rightsAlarm.setChecked(true);
+                    continue;
+                }
+            }
+        }
+        if (mUser.userLevel.equals("1")) {
+            update_rbLevelOne.setChecked(true);
+        } else {
+            update_rbLevelTwo.setChecked(true);
+        }
+
+        String[] userRanges = mUser.userRange.split(",");
+        if (userRanges != null && userRanges.length > 0) {
+            List<String> rangList = Arrays.asList(userRanges);
+            for (int i = 0; i < update_userRange.getChildCount(); i++) {
+                CheckBox child = (CheckBox) update_userRange.getChildAt(i);
+                String childText = child.getText().toString();
+                if (rangList.contains(childText)) {
+                    child.setChecked(true);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.update_cancel:
+                finish();
+                break;
+            case R.id.update_update:
+                updateUser();
+                break;
+        }
+
+    }
+
+    private void updateUser() {
+        String userName = update_userName.getText().toString();
+        String pwd = update_userPassword.getText().toString();
+        if (TextUtils.isEmpty(pwd))
+            return;
+        String rights = "";
+        if (update_rightsStation.isChecked())
+            rights += update_rightsStation.getText().toString();
+        if (update_rightsPower.isChecked())
+            rights += TextUtils.isEmpty(rights) ? update_rightsPower.getText().toString() : "," + update_rightsPower.getText().toString();
+        if (update_rightsAlarm.isChecked())
+            rights += TextUtils.isEmpty(rights) ? update_rightsAlarm.getText().toString() : "," + update_rightsAlarm.getText().toString();
+        if (TextUtils.isEmpty(rights))
+            return;
+        String range = "";
+        for (int i = 0; i < update_userRange.getChildCount(); i++) {
+            CheckBox child = (CheckBox) update_userRange.getChildAt(i);
+            if (child.isChecked())
+                range += TextUtils.isEmpty(range) ? child.getText().toString() : "," + child.getText().toString();
+        }
+        if (TextUtils.isEmpty(range))
+            return;
+        String level = "0";
+        if (update_rbLevelOne.isChecked())
+            level = "1";
+        updateUser(userName, pwd, rights, range, level);
+    }
+
 
     private void updateUser(String userName, String password, String rights, String range, String level) {
         if (TextUtils.isEmpty(userName) || TextUtils.isEmpty(rights) || TextUtils.isEmpty(range) || TextUtils.isEmpty(level))
@@ -276,6 +190,7 @@ public class UpdateUserActivity extends AppCompatActivity {
         mTask = new UpdateUserTask(userName, password, rights, range, level);
         mTask.execute();
     }
+
 
     private class UpdateUserTask extends AsyncTask<Void, Void, String> {
 
@@ -338,5 +253,30 @@ public class UpdateUserActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+
+    private void getStation() {
+        String station = "";
+        try {
+            InputStream in = getResources().openRawResource(R.raw.stations);//获取
+            int length = in.available();
+            byte[] buffer = new byte[length];
+            in.read(buffer);
+            station = new String(buffer, "UTF-8");
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (!TextUtils.isEmpty(station)) {
+            String[] dataSet = station.split(";");
+            if (dataSet != null && dataSet.length > 0) {
+                stations = new String[dataSet.length];
+                for (int i = 0; i < dataSet.length; i++) {
+                    String[] data = dataSet[i].split(",");
+                    stations[i] = data[0];
+                }
+            }
+        }
     }
 }
